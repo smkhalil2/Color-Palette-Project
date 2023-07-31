3050ab2b231b19f0354e0919a474e407767cce78
Shadi <shadi.khalil98@gmail.com>
2023-07-30 23:15:23 -0400

    add single color palette link

---
+++ color_project/src/SingleColorPalette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function SingleColorPalette() {
+    return (
+        <div>
+            <h1>Single Color Palette</h1>
+        </div>
+    )
+}
+
+export default SingleColorPalette; 
